3c6e85e56581ab58de3b3efc1e4eda350fdc9877
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-21 19:33:05 +0300

    Fix silent error when index + shift_factor > 25

--- projects/caesar_cipher/script.rb
+++ projects/caesar_cipher/script.rb
@@ -9,6 +9,9 @@ def caesar_cipher(string, shift_factor = 0)
     next ciphered_string.push(character) if $ALPHABET.include?(character.downcase) == false
 
     cipher_character_index = $ALPHABET.index(character.downcase).to_i + shift_factor
+    if cipher_character_index > 25 # indexes are from 0-25
+      cipher_character_index = cipher_character_index - 26
+    end
 
     next ciphered_string.push($ALPHABET[cipher_character_index].upcase) if character == character.upcase
     ciphered_string.push($ALPHABET[cipher_character_index])
@@ -17,4 +20,4 @@ def caesar_cipher(string, shift_factor = 0)
   p ciphered_string.join
 end
 
-caesar_cipher("Bhat a string!", 5)
+caesar_cipher("What a string!", 5)
