6433ed8890582fecba3a6ea3f9406615a7ac42e0
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-21 19:31:43 +0300

    Add uppercase letters and special character functionality

--- projects/caesar_cipher/script.rb
+++ projects/caesar_cipher/script.rb
@@ -5,12 +5,16 @@ def caesar_cipher(string, shift_factor = 0)
   ciphered_string = []
 
   string_to_array.map do |character|
+
+    next ciphered_string.push(character) if $ALPHABET.include?(character.downcase) == false
+
     cipher_character_index = $ALPHABET.index(character.downcase).to_i + shift_factor
 
+    next ciphered_string.push($ALPHABET[cipher_character_index].upcase) if character == character.upcase
     ciphered_string.push($ALPHABET[cipher_character_index])
   end
 
   p ciphered_string.join
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher("Bhat a string!", 5)
