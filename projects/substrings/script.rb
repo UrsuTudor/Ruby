4c1a50642a1fb1a0057ee55c781c2c70929f4fd8
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-22 16:54:36 +0300

    Check if groups of letters from string match words from an array

---
+++ projects/substrings/script.rb
@@ -0,0 +1,18 @@
+require "pry-byebug"
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+  occurences = Hash.new(0)
+  string_to_array = string.downcase.split
+
+  #goes through dictionary and checks if any of the words are included in elements of the given string
+  dictionary.each do |word|
+    string_to_array.each do |element|
+      occurences[word] +=1 if element.include?(word)
+    end
+  end
+  p occurences
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
