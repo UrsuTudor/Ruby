f9379b0cd3a376c8140f74c638eb9e0cd6963162
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-23 18:47:34 +0300

    Optimize sort
    Second optimization that stops the sort from iterating over the array
    if iterating is no longer needed. It does this by counting the number
    of shifts the last iteration had to perform and breaking out of the loop
    if no shifts were required.

--- projects/bubble_sort/script.rb
+++ projects/bubble_sort/script.rb
@@ -1,9 +1,12 @@
 require "pry-byebug"
 
 def bubble_sort(array)
-  sort_iterations = 0
-
+  sort_iterations = 0 
   array.length.times do 
+    #counter that allows the stopping of the function if no 
+    #more shifts were needed
+    shifts_on_iteration = 0
+
     array.each do |number|
       current_element_index = array.index(number)
       next_element_index = array.index(number) + 1
@@ -16,11 +19,13 @@ def bubble_sort(array)
       if number > array[next_element_index]
         array[current_element_index] = array[next_element_index]
         array[next_element_index] = number
+        shifts_on_iteration += 1
       end
     end
     sort_iterations += 1
+    break if shifts_on_iteration == 0
   end
-  p array
+  return array
 end
 
 bubble_sort([4,3,78,2,0,2])
