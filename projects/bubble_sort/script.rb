37a6495f52e42f903d7276f1d102aa707a1973e9
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-23 18:39:44 +0300

    Optimize sort
    By no longer checking elements that were already pushed to the end
    by the function, since it is clear that they are bigger.

--- projects/bubble_sort/script.rb
+++ projects/bubble_sort/script.rb
@@ -1,22 +1,24 @@
 require "pry-byebug"
 
 def bubble_sort(array)
+  sort_iterations = 0
+
   array.length.times do 
     array.each do |number|
       current_element_index = array.index(number)
       next_element_index = array.index(number) + 1
   
-      #prevents error when we reach the end of the array, because at that point
-      #array.index(number + 1) would go beyond the length of the array and would 
-      #return nil
+      #prevents error at the end of the array
       break if next_element_index > array.length - 1
-
+      #breaks if the next element is one that was already pushed to the end by the sort
+      break if next_element_index == array.length - sort_iterations
       #handles the sorting
       if number > array[next_element_index]
         array[current_element_index] = array[next_element_index]
         array[next_element_index] = number
       end
     end
+    sort_iterations += 1
   end
   p array
 end
