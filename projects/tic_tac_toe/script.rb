1faad4a8bff88eb6aa208cb1c6b033b0c185f162
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-29 17:23:35 +0300

    Prevent players from replacing symbols

--- projects/tic_tac_toe/script.rb
+++ projects/tic_tac_toe/script.rb
@@ -18,14 +18,19 @@ class Rows
     puts display.join
   end
 
-  private
+  # private
 
   attr_reader :display
 end
 
 # class of rows that can contain 'x' or 'o'
 class GameRow < Rows
-  def player_choice(index, choice)
+  def place_player_choice(index, choice)
+    if index > 4
+      p 'ERROR: choose a number between 1 and 3'
+      return
+    end
+
     # necessary because 1 and 3 are the indexes of the outline bars
     index = 0 if index == 1
     index = 4 if index == 3
@@ -43,7 +48,55 @@ end
 class OutlineRow < Rows
 end
 
-outline_row = OutlineRow.new(['---', '+', '---', '+', '---'])
-row1 = GameRow.new(['   ', '|', '   ', '|', '   '])
-row2 = GameRow.new(['   ', '|', '   ', '|', '   '])
-row3 = GameRow.new(['   ', '|', '   ', '|', '   '])
+def play_game
+  winner_declared = false
+  round_counter = 0
+
+  outline_row = OutlineRow.new(['---', '+', '---', '+', '---'])
+  row1 = GameRow.new(['   ', '|', '   ', '|', '   '])
+  row2 = GameRow.new(['   ', '|', '   ', '|', '   '])
+  row3 = GameRow.new(['   ', '|', '   ', '|', '   '])
+
+  until winner_declared == true
+    if round_counter.even?
+      puts "X, it's your turn! Choose a row and empty spot in which to place your mark!(row, spot)"
+      player_choice = gets.chomp.split(',')
+
+      working_row = row1 if player_choice[0] == '1'
+      working_row = row2 if player_choice[0] == '2'
+      working_row = row3 if player_choice[0] == '3'
+
+      # prevents replacement of symbols
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '1' && working_row.display[0] != '   '
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '2' && working_row.display[2] != '   '
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '3' && working_row.display[4] != '   '
+
+      working_row.place_player_choice(player_choice[1].to_i, 'x')
+    end
+
+    if round_counter.odd?
+      puts "O, it's your turn! Choose a row and empty spot in which to place your mark!(row, spot)"
+      player_choice = gets.chomp.split(',')
+
+      working_row = row1 if player_choice[0] == '1'
+      working_row = row2 if player_choice[0] == '2'
+      working_row = row3 if player_choice[0] == '3'
+
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '1' && working_row.display[0] != '   '
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '2' && working_row.display[2] != '   '
+      next puts 'ERROR: choose an empty spot!' if player_choice[1] == '3' && working_row.display[4] != '   '
+
+      working_row.place_player_choice(player_choice[1].to_i, 'o')
+    end
+
+    row1.display_row
+    outline_row.display_row
+    row2.display_row
+    outline_row.display_row
+    row3.display_row
+
+    round_counter += 1
+  end
+end
+
+play_game
