115f9e33a1bbb9eb8df7737d12f5dbc6d388418c
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-30 20:58:00 +0300

    Add a way to give the player feedback by looping through code and guess

--- projects/mastermind/script.rb
+++ projects/mastermind/script.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal:true
 
+require "pry-byebug"
+
 class Board
   def initialize
     @game_board = Array.new(8) { Array.new(4, '   ') }
@@ -23,42 +25,62 @@ class ComputerPlayer
   attr_reader :colors
 
   def generate_code
-    code = []
-    4.times do
-      code.push(colors[rand(0..3)])
-    end
-
-    p code
+    computer_code = []
+    4.times { computer_code.push(colors[rand(0..3)]) }
+    p computer_code
   end
 end
 
 class HumanPlayer
-  def get_player_code
-    puts "Take a guess!"
-    p player_code = gets.chomp
-    player_code
+  def player_guess
+    puts 'Take a guess! The colors are: red, blue, yellow and green.'
+    player_guess = gets.chomp.split
   end
 end
 
 class Game 
   def initialize
-    @board = Board.new 
+    @board = Board.new.display_game_board
     @computer = ComputerPlayer.new
+    @computer_code = computer.generate_code
     @player = HumanPlayer.new
+    @player_guess = player.player_guess
+    @winner = false
   end
 
-  attr_reader :board, :computer, :player
+  attr_reader :board, :computer, :player, :computer_code
+  attr_accessor :player_guess, :winner
 
   def play
-    board.display_game_board
-    computer.generate_code
-    player.get_player_code
+    # will need to be in a loop, as opposed to game board and generate code
+    compare_codes
+    winner?
+  end
+
+  def compare_codes
+    computer_code.each_with_index do |color, color_index|
+      player_guess.each do |guess|
+        # exact spot
+        if guess == color && computer_code[color_index] == player_guess[color_index]
+          p "#{guess.upcase} from column #{color_index + 1} is in the right spot!"
+          break
+        end
+
+        # right color, wrong spot
+        if guess == color && computer_code[color_index] != player_guess[color_index]
+          p "There is one/one more instance of the color #{guess.upcase}, but in a different column!"
+          break
+        end
+      end
+    end
+  end
+
+  def winner?
+    if player_guess == computer_code
+      winner = true
+      p 'Congratulations, you cracked the code!'
+    end
   end
 end
 
 Game.new.play
-
-# need to make player algorhitm that allows player to select code
-# get player choice
-# check player choice, update row and give feedback
-# move on to row 2, probably with a loop on the rows or an i+ that changes board[i] to hop on the next row with the update
