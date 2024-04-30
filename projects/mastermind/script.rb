ff2efb3e83ea04e41f360dd1e5d8388cca9fef45
UrsuTudor <tudor_ursu90@yahoo.com>
2024-04-30 19:10:32 +0300

    Add Game and HumanPlayer classes + a way to get user input

--- projects/mastermind/script.rb
+++ projects/mastermind/script.rb
@@ -32,10 +32,33 @@ class ComputerPlayer
   end
 end
 
-board = Board.new
+class HumanPlayer
+  def get_player_code
+    puts "Take a guess!"
+    p player_code = gets.chomp
+    player_code
+  end
+end
 
-board.display_game_board
+class Game 
+  def initialize
+    @board = Board.new 
+    @computer = ComputerPlayer.new
+    @player = HumanPlayer.new
+  end
 
-cp = ComputerPlayer.new
+  attr_reader :board, :computer, :player
 
-cp.generate_code
+  def play
+    board.display_game_board
+    computer.generate_code
+    player.get_player_code
+  end
+end
+
+Game.new.play
+
+# need to make player algorhitm that allows player to select code
+# get player choice
+# check player choice, update row and give feedback
+# move on to row 2, probably with a loop on the rows or an i+ that changes board[i] to hop on the next row with the update
