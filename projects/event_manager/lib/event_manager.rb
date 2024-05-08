5cebd008ce5af68567d6274e0accb9d39a526da7
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-08 18:23:17 +0300

    Add method to clean zipcodes from csv

--- projects/event_manager/lib/event_manager.rb
+++ projects/event_manager/lib/event_manager.rb
@@ -9,8 +9,13 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
