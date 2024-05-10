c69244235d9598acd6b26faf00dffe80e755e8f7
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-10 19:26:20 +0300

    Display a dotted line on which guesses will be placed

--- projects/hangman/lib/hangman.rb
+++ projects/hangman/lib/hangman.rb
@@ -3,17 +3,21 @@
 class Dictionary 
   def initialize
     @dictionary_words = File.read('google-10000-english-no-swears.txt').split
+    @current_game_word = game_word
   end
 
-  def current_game_word
+  attr_reader :current_game_word
+
+  private
+
+  def game_word
     usable_words = @dictionary_words.filter { |word| word.length >= 5 && word.length <= 12 }
-    current_game_word = usable_words[rand(0..7557)]
-    current_game_word
+    @current_game_word = usable_words[rand(0..7557)].split('')
   end
 end
 
 class Gallow
-  def initialize 
+  def initialize
     @number_of_mistakes = 0
     @gallows = [['  ________'], ['   |   |'], ['   |'], ['   |'], ['   |'], [' ___________']]
   end
@@ -50,7 +54,20 @@ class Game
     @current_game_word = Dictionary.new.current_game_word
     @gallow = Gallow.new
     @gallow.draw_gallows
+    p @current_game_word
+  end
+
+  attr_reader :current_game_word
+
+  def display_guesses
+    dotted_line = []
+    current_game_word.length.times { dotted_line.push('_') }
+    puts " \n#{dotted_line.join}"
+  end
+
+  def play
+    display_guesses
   end
 end
 
-Game.new
+Game.new.play
