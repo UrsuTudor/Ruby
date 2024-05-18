6c1f7211e1666cc2ff4d24ffa2dd324e6be600dd
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-18 20:51:04 +0300

    Add helper merge

--- projects/merge_sort/script.rb
+++ projects/merge_sort/script.rb
@@ -5,4 +5,17 @@ def merge_sort(arr)
   right = merge_sort(arr[middle..])
 end
 
+def merge(left, right)
+  sorted = []
+
+  until left.empty? || right.empty?
+    if left.first < right.first
+      sorted.push(left.shift)
+    elsif right.first < left.first
+      sorted.push(right.shift)
+    end
+  end
+  p sorted.concat(left).concat(right)
+end
+
 merge_sort([1, 2, 3, 4, 5, 6])
