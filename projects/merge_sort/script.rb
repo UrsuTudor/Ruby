a8587a756247d49330e85a1da143b94192436b92
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-18 21:03:36 +0300

    Combine sort with merge_sort

--- projects/merge_sort/script.rb
+++ projects/merge_sort/script.rb
@@ -1,8 +1,13 @@
+require 'pry-byebug'
+
 def merge_sort(arr)
   return arr if arr.length <= 1
+
   middle = arr.length / 2
   left = merge_sort(arr[0...middle])
   right = merge_sort(arr[middle..])
+
+  merge(left, right)
 end
 
 def merge(left, right)
@@ -15,7 +20,8 @@ def merge(left, right)
       sorted.push(right.shift)
     end
   end
+
   p sorted.concat(left).concat(right)
 end
 
-merge_sort([1, 2, 3, 4, 5, 6])
+merge_sort([2, 1, 3, 6, 4, 5])
