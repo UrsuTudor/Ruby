09666898f9e2cdae9e54cc127d8f7ce483885e27
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-20 20:10:22 +0300

    Add notes

---
+++ notes/data_structures/notes.rb
@@ -0,0 +1,26 @@
+# Data structures are a collection of data values, the relationships among them 
+# and the functions that can be applied to the data.
+
+# TYPES:
+  # array
+  # linked list
+    # linear collection where each element has a value attached to it and points to the next element
+    # values can be inserted and removed without relocating the rest of the list like in an array
+    # slower than arrays on certain operations
+  # record
+    # aggregate data structure
+    # a record is a value that contains other values
+  # hash table
+  # graph
+    # collections of vertices(nodes) connected by edges, representing relationships between entities
+    # directed or undirected, cyclical or acyclical
+  # stack/queue
+    # abstract data types implemented using arrays or linked lists
+    # push and pop add or remove an element at the top
+    # work by the LIFO principle (last in, first out)
+  # trees
+    # hierarchical organization of elements
+    # nodes connected by edges, where one node is the root while others form subtrees
+  # trie
+    # specialized tree for retrieval of strings
+    # strings stored as nodes, with each edge representing a character
