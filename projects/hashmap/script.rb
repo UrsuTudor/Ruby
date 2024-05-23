846486a96cdf1d9b46723d7da295a29610fa9a85
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-23 20:56:26 +0300

    Add update functionality to set method
    And each_with_index loop ran on the bucket checks if the bucket doesn't
    already contain a node with the specified key. If it does, it deletes it
    and replaces the old value linked to the key with the new value.

---
+++ projects/hashmap/script.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+
+# linked list and methods on it
+class LinkedList
+  def initialize
+    @list = []
+  end
+
+  def append(value)
+    list.push(Node.new(value, list[0]))
+  end
+
+  def preppend(value)
+    list.unshift(Node.new(value, list[0]))
+  end
+
+  def size
+    list.length
+  end
+
+  def head
+    list[0]
+  end
+
+  def tail
+    list[-1]
+  end
+
+  def at(index)
+    list[index]
+  end
+
+  def pop
+    list.pop
+  end
+
+  def contains?(value)
+    list.each do |node|
+      return true if node.value == value
+    end
+
+    false
+  end
+
+  def find(value)
+    list.each_with_index do |node, index|
+      return index if node.value == value
+    end
+
+    nil
+  end
+
+  def to_s
+    stringed = ''
+    list.each do |node|
+      stringed << "(#{node.value}) -> "
+    end
+
+    stringed << 'nil'
+    stringed
+  end
+
+  def insert_at(value, insert_index)
+    list.insert(insert_index, Node.new(value, list[insert_index]))
+
+    list.each_with_index do |node, node_index|
+      node.next_node = list[node_index + 1] if node_index < insert_index
+    end
+
+    list
+  end
+
+  def delete_at(delete_index)
+    list.delete_at(delete_index)
+
+    list[delete_index - 1].next_node = list[delete_index] if !delete_index.zero?
+
+    list
+  end
+
+  def increment_next_node
+    list.each_with_index do |node, node_index|
+      node.next_node = list[node_index + 1]
+    end
+  end
+
+  def each_with_index
+    list_length = list.length
+    index = 0
+    until index == list_length
+      yield(list[index], index)
+      index += 1
+    end
+    self
+  end
+
+  private
+
+  attr_accessor :list
+end
+
+# simple node
+class Node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def key
+    key = value.each { |key, value| return key}
+  end
+  attr_accessor :value, :next_node
+end
+
+class HashMap
+  def initialize
+    @buckets = Array.new(16)
+    @capacity = 16
+    @load_factor = 0.75
+  end
+
+  attr_accessor :capacity, :buckets
+  attr_reader :load_factor
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+
+  def set(key, value)
+    hash_object = { "#{key}": value }
+    index = hash(key) % capacity
+
+    raise IndexError if index.negative? || index >= @buckets.length
+
+    if buckets[index].nil?
+      buckets[index] = LinkedList.new
+      buckets[index].append(hash_object)
+      return
+    end
+
+    # updates node if the key is a duplicate; I chose an each_with_index because 
+    # the indexes are needed for the #delete_at and #insert_at methods
+    buckets[index].each_with_index do |node, node_index|
+      if node.key.to_s == key
+        buckets[index].delete_at(node_index)
+        buckets[index].insert_at(hash_object, node_index)
+        p buckets[index]
+        return
+      end
+    end
+
+    # appends the new hash_objects if the two ifs above don't return
+    buckets[index].append(hash_object)
+    buckets[index].increment_next_node
+  end
+end
