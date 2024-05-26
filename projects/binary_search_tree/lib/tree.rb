58a7d5be309320a2f2a6ca72ec591a860258edd3
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-26 21:34:54 +0300

    Add #build_tree method

--- projects/binary_search_tree/lib/tree.rb
+++ projects/binary_search_tree/lib/tree.rb
@@ -9,4 +9,19 @@ class Tree
   protected
 
   attr_accessor :root
+
+  def build_tree(arr = self.sorted_arr)
+    return nil if arr.empty?
+
+    mid = arr.length / 2
+    self.root = Node.new(arr[mid], build_tree(arr[0...mid]), build_tree(arr[mid + 1..]))
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
