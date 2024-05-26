8eb6aa682e399342a6d4d3ccb6f7f1d0007722be
UrsuTudor <tudor_ursu90@yahoo.com>
2024-05-26 21:34:13 +0300

    Add Node and Tree classes

--- projects/binary_search_tree/lib/tree.rb
+++ projects/binary_search_tree/lib/tree.rb
@@ -8,48 +8,5 @@ class Tree
 
   protected
 
-  attr_accessor :arr, :balanced_tree, :root, :sorted_arr
-
-  def build_tree(arr = self.sorted_arr)
-    return nil if arr.empty?
-
-    mid = arr.length / 2
-    self.root = Node.new(arr[mid], build_tree(arr[0...mid]), build_tree(arr[mid + 1..]))
-
-    root
-  end
-
-  public
-
-  def insert(val, root = self.root)
-    return root = Node.new(val) if root.nil?
-    return root if val == root.data
-
-    if val > root.data
-      root.right = insert(val, root.right)
-    elsif val < root.data
-      root.left = insert(val, root.left)
-    end
-
-    root
-  end
-
-  def delete(val, root = self.root)
-    # binding.pry
-    return nil if val == root.data
-
-    if val > root.data
-      root.right = delete(val, root.right)
-    elsif val < root.data
-      root.left = delete(val, root.left)
-    end
-
-    root
-  end
-
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-  end
+  attr_accessor :root
 end
--- projects/binary_search_tree/main.rb
+++ projects/binary_search_tree/main.rb
@@ -2,6 +2,4 @@ require_relative 'lib/tree'
 require_relative 'lib/node'
 
 tree = Tree.new([5, 6, 2, 3, 1, 4])
-tree.delete(5)
-p tree
 tree.pretty_print
