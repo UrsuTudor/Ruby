e725f97e96ee3c5b3ea3885b6c7b5dde1071adf7
UrsuTudor <tudor_ursu90@yahoo.com>
2024-06-04 20:08:20 +0300

    Remove global variable

--- projects/caesar_cipher/script.rb
+++ projects/caesar_cipher/script.rb
@@ -1,24 +1,23 @@
-$ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-
 def caesar_cipher(string, shift_factor = 0)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
   string_to_array = string.split('')
   ciphered_string = []
 
   string_to_array.map do |character|
-    #pushes special characters into the string automatically
-    next ciphered_string.push(character) if $ALPHABET.include?(character.downcase) == false
+    # pushes special characters into the string automatically
+    next ciphered_string.push(character) if alphabet.include?(character.downcase) == false
 
-    cipher_character_index = $ALPHABET.index(character.downcase).to_i + shift_factor
-    #wraps back to a if cipher_character_index would go beyond z
-    if cipher_character_index > 25 
-      cipher_character_index = cipher_character_index - 26 
-    end
+    cipher_character_index = alphabet.index(character.downcase).to_i + shift_factor
+    # wraps back to 'a' if cipher_character_index would go beyond 'z'
+    cipher_character_index -= 26 if cipher_character_index > 25
 
-    next ciphered_string.push($ALPHABET[cipher_character_index].upcase) if character == character.upcase 
-    ciphered_string.push($ALPHABET[cipher_character_index])
+    next ciphered_string.push(alphabet[cipher_character_index].upcase) if character == character.upcase
+
+    ciphered_string.push(alphabet[cipher_character_index])
   end
 
   p ciphered_string.join
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
