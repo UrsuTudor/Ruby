8ad408054a7074a55252638c437a8b02020803df
UrsuTudor <tudor_ursu90@yahoo.com>
2024-06-07 20:14:28 +0300

    Add #place_color method
    The method asks for player input, formats it into an array of integers
    and assigns it to the corresponding row/col pair depending on the round.
    
    Before doing that, it makes sure that the space is empty. If it isn't, it
    puts an error message and asks for different input.

--- projects/connect_four/lib/game.rb
+++ projects/connect_four/lib/game.rb
@@ -1,9 +1,13 @@
+require_relative 'space'
+
 class Game
   def initialize
     @board = Array.new(6) { Array.new(7) }
+    @round = 0
   end
 
   attr_reader :board
+  attr_accessor :round
 
   def display_board
     puts ''
@@ -13,5 +17,12 @@ class Game
       end
       puts "\n ----------------------------------"
     end
+  end
+
+  def place_color(row, col)
+    return 'That space is already occupied.' unless board[row - 1][col - 1].nil?
+
+    board[row - 1][col - 1] = Space.new('r') if round.even? || round.zero?
+    board[row - 1][col - 1] = Space.new('y') if round.odd?
   end
 end
--- projects/connect_four/spec/game_spec.rb
+++ projects/connect_four/spec/game_spec.rb
@@ -1,16 +1,55 @@
 require './lib/game'
 
 describe Game do
-  subject(:board) { described_class.new }
+  subject(:game) { described_class.new }
 
-  before do
-    allow(board.instance_variable_get(:@board)).to receive(:each)
+  describe 'display_game' do
+    before do
+      allow(game.instance_variable_get(:@board)).to receive(:each)
+    end
+
+    it 'displays the arrays of the game in the console' do
+      expect(game.instance_variable_get(:@board)).to receive(:each).once
+      game.display_board
+    end
   end
 
-  describe 'display_board' do
-    it 'displays the arrays of the board in the console' do
-      expect(board.instance_variable_get(:@board)).to receive(:each).once
-      board.display_board
+  describe 'place_color' do
+    context 'when called with (1, 1)' do
+      it 'changes the first element of the first row' do
+        board = game.instance_variable_get(:@board)
+        game.place_color(1, 1)
+        expect(board[0][0]).to be_instance_of(Space)
+      end
+
+      it 'changes the first element of the first row to a #Space belonging to red if the round is 0' do
+        board = game.instance_variable_get(:@board)
+        game.round = 0
+        game.place_color(1, 1)
+        expect(board[0][0].instance_variable_get(:@player)).to eq('r')
+      end
+
+      it 'changes the first element of the first row to a #Space belonging to red if the round is even' do
+        board = game.instance_variable_get(:@board)
+        game.round = 2
+        game.place_color(1, 1)
+        expect(board[0][0].instance_variable_get(:@player)).to eq('r')
+      end
+
+      it 'changes the first element of the first row to a #Space belonging to yellow if the round is odd' do
+        board = game.instance_variable_get(:@board)
+        game.round = 3
+        game.place_color(1, 1)
+        expect(board[0][0].instance_variable_get(:@player)).to eq('y')
+      end
+
+      it 'returns if the space is already occupied' do
+        board = game.instance_variable_get(:@board)
+        expect(game).to receive(:place_color).with(1, 1).and_return(board[0][0])
+        game.place_color(1, 1)
+        expect(game).to receive(:place_color).with(1, 1).and_return('That space is already occupied.')
+        game.place_color(1, 1)
+      end
     end
   end
 end
